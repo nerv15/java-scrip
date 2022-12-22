7b99c687fe1d9722251284765910f347f9d8e53d
nerv <nerv@gmail.com>
2022-12-22 16:30:10 +0500

    js1

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// var first_name = 'Irina'
+// console.log() - Функция вывода данных консоле
+// console.log(first_name)
+// alert() - Функция вывода данных в окне браузера
+// alert(first_name)
+
+// prompt() - функция которая принимает данные у пользователя
+var firstName = prompt('Введите свое имя')
+console.log('Меня зовут ' + firstName);
+var age = prompt('Введите свой возрост')
+console.log('Восраст: ' + age);
+var answer = prompt('Решите пример: 10 + 3 = ?')
+console.log('Пример 1 : 10 + 3 = + ( 10 + 3 ) Ваш ответ: ' + answer)
+var answer = prompt('Решите пример : 10 - 3 = ?')
+console.log('Пример 2 : 10 - 3 = + ( 10 - 3 ) Ваш ответ: ' + answer);
+var answer = prompt('Решите пример : 10 * 3 = ?')
+console.log('Пример 3 : 10 * 3 = + (10 * 3) Ваш ответ:' + answer);
+var answer = promt('Решите пример : 10 / 3 = ?')
+console.log('Пример 4 : 10 / 3 = + (10 / 3) Ваш ответ:' + answer);
+var answer = promt('Решите пример : 10 % 3 = ?')
+console.log('Пример 5 : 10 % 3 = + (10 % 3) Ваш ответ:' + answer);
+// console.log('Irina')
+// console.log('зовут')
+// console.log(20)
+
+
+
+
+
+
+
+
+ 
+
+ 
